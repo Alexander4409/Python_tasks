d07c6a4b4eaedd73c5ae35f26be0076675286e1c
Alexander <120810491+Alexander4409@users.noreply.github.com>
2023-04-15 09:31:54 +0700

    This code is an example of working with classes

---
+++ HW_Tailakov.A.A.Class_Part1.py
@@ -0,0 +1,204 @@
+# #Task 1
+# class Car:
+#     def __init__(self):
+#         self.model_name = ''
+#         self.year = 0
+#         self.manufacturer = ''
+#         self.engine_size = 0
+#         self.color = ''
+#         self.price = 0
+#
+#     def set_model_name(self, model_name):
+#         self.model_name = model_name
+#
+#     def set_year(self, year):
+#         self.year = year
+#
+#     def set_manufacturer(self, manufacturer):
+#         self.manufacturer = manufacturer
+#
+#     def set_engine_size(self, engine_size):
+#         self.engine_size = engine_size
+#
+#     def set_color(self, color):
+#         self.color = color
+#
+#     def set_price(self, price):
+#         self.price = price
+#
+#     def get_model_name(self):
+#         return self.model_name
+#
+#     def get_year(self):
+#         return self.year
+#
+#     def get_manufacturer(self):
+#         return self.manufacturer
+#
+#     def get_engine_capacity(self):
+#         return self.engine_size
+#
+#     def get_color(self):
+#         return self.color
+#
+#     def get_price(self):
+#         return self.price
+#
+#     def input_car_data(self):
+#         self.set_model_name(input("Input the model of the vehicle: "))
+#         self.set_year(int(input("Enter year of manufacture: ")))
+#         self.set_manufacturer(input("Enter the manufacturer: "))
+#         self.set_engine_size(float(input("Enter the engine capacity: ")))
+#         self.set_color(input("Enter the colour of the vehicle: "))
+#         self.set_price(float(input("Enter the price: ")))
+#
+#     def output_car_data(self):
+#         print(f"Model: {self.get_model_name()}")
+#         print(f"The year of manufacture: {self.get_year()}")
+#         print(f"Manufacturer: {self.get_manufacturer()}")
+#         print(f"Engine capacity: {self.get_engine_capacity()}")
+#         print(f"Colour of the vehicle: {self.get_color()}")
+#         print(f"Price: {self.get_price()}")
+#
+# my_car = Car()
+#
+# my_car.input_car_data()
+# my_car.output_car_data()
+#
+# print(f"Model: {my_car.get_model_name()}")
+# print(f"The year of manufacture: {my_car.get_year()}")
+
+
+#Task 2
+# class Book:
+#     def __init__(self):
+#         self.title = ''
+#         self.year = 0
+#         self.publisher = ''
+#         self.genre = ''
+#         self.author = ''
+#         self.price = 0
+#
+#     def set_title(self, title):
+#         self.title = title
+#
+#     def set_year(self, year):
+#         self.year = year
+#
+#     def set_publisher(self, publisher):
+#         self.publisher = publisher
+#
+#     def set_genre(self, genre):
+#         self.genre = genre
+#
+#     def set_author(self, author):
+#         self.author = author
+#
+#     def set_price(self, price):
+#         self.price = price
+#
+#     def get_title(self):
+#         return self.title
+#
+#     def get_year(self):
+#         return self.year
+#
+#     def get_publisher(self):
+#         return self.publisher
+#
+#     def get_genre(self):
+#         return self.genre
+#
+#     def get_author(self):
+#         return self.author
+#
+#     def get_price(self):
+#         return self.price
+#
+#     def input_book_data(self):
+#         self.set_title(input("Enter book title: "))
+#         self.set_year(int(input("Enter year of issue: ")))
+#         self.set_publisher(input("Enter publisher: "))
+#         self.set_genre(input("Enter Genre: "))
+#         self.set_author(input("Enter author: "))
+#         self.set_price(float(input("Enter the price ")))
+#
+#     def output_book_data(self):
+#         print(f"Book title: {self.get_title()}")
+#         print(f"Year of issue: {self.get_year()}")
+#         print(f"Publisher: {self.get_publisher()}")
+#         print(f"Genre: {self.get_genre()}")
+#         print(f"Author: {self.get_author()}")
+#         print(f"Price: {self.get_price()}")
+#
+#
+# my_book = Book()
+#
+# my_book.input_book_data()
+# my_book.output_book_data()
+#
+# print(f"Book title: {my_book.get_title()}")
+# print(f"Year of issue: {my_book.get_year()}")
+
+
+#Task 3
+# class Stadium:
+#     def __init__(self):
+#         self.name = ''
+#         self.opening_date = ''
+#         self.country = ''
+#         self.city = ''
+#         self.capacity = 0
+#
+#     def set_name(self, name):
+#         self.name = name
+#
+#     def set_opening_date(self, opening_date):
+#         self.opening_date = opening_date
+#
+#     def set_country(self, country):
+#         self.country = country
+#
+#     def set_city(self, city):
+#         self.city = city
+#
+#     def set_capacity(self, capacity):
+#         self.capacity = capacity
+#
+#     def get_name(self):
+#         return self.name
+#
+#     def get_opening_date(self):
+#         return self.opening_date
+#
+#     def get_country(self):
+#         return self.country
+#
+#     def get_city(self):
+#         return self.city
+#
+#     def get_capacity(self):
+#         return self.capacity
+#
+#     def input_stadium_data(self):
+#         self.set_name(input("Enter stadium name: "))
+#         self.set_opening_date(input("Enter opening date: "))
+#         self.set_country(input("Enter country: "))
+#         self.set_city(input("Enter city: "))
+#         self.set_capacity(int(input("Enter capacity: ")))
+#
+#     def output_stadium_data(self):
+#         print(f"Stadium name: {self.get_name()}")
+#         print(f"Opening date: {self.get_opening_date()}")
+#         print(f"Country: {self.get_country()}")
+#         print(f"City: {self.get_city()}")
+#         print(f"Stadium Capacity: {self.get_capacity()}")
+#
+# my_stadium = Stadium()
+#
+# my_stadium.input_stadium_data()
+# my_stadium.output_stadium_data()
+#
+# print(f"Stadium name: {my_stadium.get_name()}")
+# print(f"Stadium Capacity: {my_stadium.get_capacity()}")
+
